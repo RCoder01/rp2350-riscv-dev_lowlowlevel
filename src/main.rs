66f6fa6b9a100f2b44f3f7c39cf1fdbc78739f38
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2026-02-08 19:52:30 -0500

    Rename registers

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
 #![no_std]
 #![no_main]
 
-const A: *mut u32 = 0x4002_80cc_usize as _;
-const B: *mut u32 = 0x4003_8068_usize as _;
-const C: *mut u32 = 0xd000_0038_usize as _;
-const XOR: *mut u32 = 0xD000_0028_usize as _;
+const GPIO25_CTRL_REG: *mut u32 = 0x4002_80cc_usize as _;
+const PADS_BANK0_GPIO25: *mut u32 = 0x4003_8068_usize as _;
+const SIO_GPIO_OE_SET: *mut u32 = 0xd000_0038_usize as _;
+const SIO_GPIO_OUT_XOR: *mut u32 = 0xD000_0028_usize as _;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    const SIO: u32 = 5;
+
+    const DRIVE_STRENGTH_12MA: u32 = 0x3 << 4;
+    const PULL_DOWN_ENABLE: u32 = 1 << 2;
     unsafe {
-        A.write_volatile(5);
-        B.write_volatile(0x34);
-        C.write_volatile(1u32 << 25);
+        GPIO25_CTRL_REG.write_volatile(SIO);
+        PADS_BANK0_GPIO25.write_volatile(DRIVE_STRENGTH_12MA | PULL_DOWN_ENABLE);
+        SIO_GPIO_OE_SET.write_volatile(1 << 25);
     }
     'outer: loop {
-        unsafe { XOR.write_volatile(1u32 << 25) };
-        let mut r = 1u32 << 25;
+        unsafe { SIO_GPIO_OUT_XOR.write_volatile(1 << 25) };
+        let mut r = 1u32 << 22;
         loop {
-            r -= 1 << 3;
+            r -= 1;
             unsafe {
                 core::arch::asm!("", options(nomem, nostack, preserves_flags));
             }
