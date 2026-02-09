433a067f16f2a7f237b369bc46a2943a5e20b25c
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2026-02-09 03:36:33 -0500

    Fix linker and read sp

--- src/main.rs
+++ src/main.rs
@@ -127,42 +127,126 @@ const fn pads_gpio_reg(pin: GpioPin) -> *mut u32 {
 }
 
 const SIO_GPIO_OE_SET: *mut u32 = 0xd000_0038_usize as _;
+const SIO_GPIO_OUT_SET: *mut u32 = 0xd000_0018_usize as _;
+const SIO_GPIO_OUT_CLR: *mut u32 = 0xd000_0020_usize as _;
 const SIO_GPIO_OUT_XOR: *mut u32 = 0xD000_0028_usize as _;
 
 fn gpio_output_enable(pin: GpioPin) {
-    if (pin as u8) < 32 {
-        unsafe { SIO_GPIO_OE_SET.write_volatile(1 << (pin as u32)) }
-    } else {
-        unsafe { SIO_GPIO_OE_SET.offset(1).write_volatile(1 << (pin as u32)) }
+    let mut addr = SIO_GPIO_OE_SET;
+    if (pin as u8) >= 32 {
+        addr = unsafe { addr.offset(1) };
+    }
+    unsafe {
+        addr.write_volatile(1 << (pin as u32));
+    }
+}
+
+fn gpio_output_set(pin: GpioPin) {
+    let mut addr = SIO_GPIO_OUT_SET;
+    if (pin as u8) >= 32 {
+        addr = unsafe { addr.offset(1) };
+    }
+    unsafe {
+        addr.write_volatile(1 << (pin as u32));
+    }
+}
+
+fn gpio_output_clear(pin: GpioPin) {
+    let mut addr = SIO_GPIO_OUT_CLR;
+    if (pin as u8) >= 32 {
+        addr = unsafe { addr.offset(1) };
+    }
+    unsafe {
+        addr.write_volatile(1 << (pin as u32));
     }
 }
 
 fn gpio_output_xor(pin: GpioPin) {
-    if (pin as u8) < 32 {
-        unsafe { SIO_GPIO_OUT_XOR.write_volatile(1 << (pin as u32)) }
-    } else {
-        unsafe { SIO_GPIO_OUT_XOR.offset(1).write_volatile(1 << (pin as u32)) }
+    let mut addr = SIO_GPIO_OUT_XOR;
+    if (pin as u8) >= 32 {
+        addr = unsafe { addr.offset(1) };
+    }
+    unsafe {
+        addr.write_volatile(1 << (pin as u32));
     }
 }
 
 const LED_PIN: GpioPin = GpioPin::GPIO25;
 
+const SIO: u32 = 5;
+
+const DRIVE_STRENGTH_12MA: u32 = 0x3 << 4;
+const PULL_DOWN_ENABLE: u32 = 1 << 2;
+
+#[unsafe(link_section = ".text._start")]
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    const SIO: u32 = 5;
-
-    const DRIVE_STRENGTH_12MA: u32 = 0x3 << 4;
-    const PULL_DOWN_ENABLE: u32 = 1 << 2;
-
     unsafe {
         gpio_ctrl_reg(LED_PIN).write_volatile(SIO);
         pads_gpio_reg(LED_PIN).write_volatile(DRIVE_STRENGTH_12MA | PULL_DOWN_ENABLE);
         gpio_output_enable(LED_PIN);
     }
 
+    let stack: usize;
+    unsafe {
+        core::arch::asm!(
+            "mv {0}, sp",
+            out(reg) stack,
+            options(nomem, nostack, preserves_flags)
+        );
+    }
+    let mut read = stack;
+    for _ in 0..32 {
+        let bit = read & 1;
+        if bit == 1 {
+            blink_1();
+        } else {
+            blink_0();
+        }
+        read >>= 1;
+    }
+    panic!();
+}
+
+fn blink_0() {
+    delay(16);
+    gpio_output_set(LED_PIN);
+    delay(4);
+    gpio_output_clear(LED_PIN);
+}
+
+fn blink_1() {
+    delay(16);
+    gpio_output_set(LED_PIN);
+    delay(4);
+    gpio_output_clear(LED_PIN);
+    delay(4);
+    gpio_output_set(LED_PIN);
+    delay(4);
+    gpio_output_clear(LED_PIN);
+}
+
+fn delay(units: u32) {
+    let count = units << 18;
+    for _ in 0..count {
+        unsafe {
+            core::arch::asm!("", options(nomem, nostack, preserves_flags));
+        }
+    }
+}
+
+#[panic_handler]
+fn panic_handler(_: &core::panic::PanicInfo) -> ! {
+    unsafe {
+        gpio_ctrl_reg(LED_PIN).write_volatile(SIO);
+        pads_gpio_reg(LED_PIN).write_volatile(DRIVE_STRENGTH_12MA | PULL_DOWN_ENABLE);
+        gpio_output_enable(LED_PIN);
+    }
+
+    const PERIOD: usize = 1 << 18;
     'outer: loop {
         gpio_output_xor(LED_PIN);
-        let mut r = 1u32 << 20;
+        let mut r = PERIOD;
         loop {
             r -= 1;
             unsafe {
@@ -173,9 +257,4 @@ pub extern "C" fn _start() -> ! {
             }
         }
     }
-}
-
-#[panic_handler]
-fn panic_handler(_: &core::panic::PanicInfo) -> ! {
-    loop {}
 }
